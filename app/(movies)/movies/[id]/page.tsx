8270de1c535cc8727eb7831ccb5ddd202f786b2b
shkim0060 <shkim0060@raoncorp.com>
2024-10-28 13:19:53 +0900

    + params id error edit

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,20 +2,22 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const { id } = await params; // params 자체를 await하여 id에 접근
-  const movie = await getMovie(id)
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
 export default async function MovieDetailPage({
-  params,
+  params: { id },
 }: {
   params: { id: string };
 }) {
-  const { id } = await params; // params 자체를 await하여 id에 접근
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info...</h1>}>
